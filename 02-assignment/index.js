ba2b29784c6d632ce971dc5c32fcc154f78e0415
ghost <keshav.bgir.980@gmail.com>
2025-08-09 00:43:47 +0530

    add app.js and demo.js in folder 01-assignment, done some assignment 02 and create assignment 03

---
+++ 02-assignment/index.js
@@ -0,0 +1,12 @@
+// Write your code below:
+const header = document.getElementById('main-heading');
+header.innerHTML = 'Fruit World';
+header.style.color = 'orange';
+const div = document.getElementById('header');
+div.style.backgroundColor = 'green';
+div.style.borderBottom = '1px solid orange';
+
+const subHeader = document.getElementById('basket-heading');
+subHeader.style.color = 'green';
+const pEl = document.getElementById('thanks');
+pEl.innerHTML = '<p>Please visit us again</p>';
