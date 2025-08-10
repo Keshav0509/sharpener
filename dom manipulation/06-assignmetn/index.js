b7a1da09fb15ca7c7fbc084940024a61c5410319
ghost <keshav.bgir.980@gmail.com>
2025-08-10 12:09:33 +0530

    06-assignment created

---
+++ dom manipulation/06-assignmetn/index.js
@@ -0,0 +1,7 @@
+/*
+1. Inside the first Div, after the main heading add a sub-heading (h3 tag) "Buy high quality organic fruits online".
+2. Make the sub-heading text italic.
+3. Inside the second Div, before the unordered list add a paragraph tag showing "Total fruits: 4".
+4. On this paragraph tag set an id of "fruits-total".
+*/
+
