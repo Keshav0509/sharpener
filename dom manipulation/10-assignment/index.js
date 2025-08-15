e52fbefa0357d4be48d62efeafd018e65fb116ca
ghost <keshav.bgir.980@gmail.com>
2025-08-15 20:10:32 +0530

    10-assignment completed final

--- dom manipulation/10-assignment/index.js
+++ dom manipulation/10-assignment/index.js
@@ -1,89 +1,86 @@
 /*
-Deliverables:
-1. Use DOM Manipulation to add another input element inside form, before the button. This input element will take the description of the fruit.
+  Deliverables:
+  1. Use DOM Manipulation to add another input element inside form, before the button. This input element will take the description of the fruit.
 
-2. Use DOM Manipulation to show the fruit description (in italics) on screen alongwith the fruit name. This description should be shown in the next line (HINT: To show description on next line you can make use of the paragraph tag).
+  2. Use DOM Manipulation to show the fruit description (in italics) on screen alongwith the fruit name. This description should be shown in the next line (HINT: To show description on next line you can make use of the paragraph tag).
 
-3. Now, create a filter that shows only those fruits whose either name or description or both matches the entered text.
+  3. Now, create a filter that shows only those fruits whose either name or description or both matches the entered text.
 
-Note: The input element that you will create using DOM manipulation to collect the description of fruit, must be given an id “description”.
+  Note: The input element that you will create using DOM manipulation to collect the description of fruit, must be given an id “description”.
 
-If you are getting any errors try debugging the error, after all no one becomes a developer without making mistakes and then debugging. Watch this video to understand the different ways of debugging.
+  If you are getting any errors try debugging the error, after all no one becomes a developer without making mistakes and then debugging. Watch this video to understand the different ways of debugging.
 */
 
+// Add input element inside form, before button, to take fruit description
 const form = document.querySelector('form');
+const button = form.querySelector('button');
 const inputEl = document.createElement('input');
-inputEl.classList.add('fruit-describe');
+inputEl.classList.add('fruit-description');
+inputEl.id = 'description';
 inputEl.setAttribute('placeholder', 'Fruit Description');
-const fruitToAdd = document.getElementById('fruit-to-add');
-const addBtn = document.querySelector('button');
-form.insertBefore(inputEl, addBtn);
+form.insertBefore(inputEl, button);
 
-const filter = document.querySelector('#filter');
-
-// add new button in li element
-function addBtns(li){
-  const div = document.createElement('div');
-  div.classList.add('btn-container');
-
+// add functional buttons in list item.
+function addBtnToLi(li) {
   const editBtn = document.createElement('button');
   editBtn.classList.add('edit-btn');
   editBtn.textContent = 'Edit';
-  div.appendChild(editBtn);
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.classList.add('delete-btn');
-  deleteBtn.textContent = 'x';
+  li.appendChild(editBtn);
   
-  div.appendChild(deleteBtn);
-  li.appendChild(div);
+  const delBtn = document.createElement('button');
+  delBtn.classList.add('delete-btn');
+  delBtn.textContent = 'x';
+  li.appendChild(delBtn); 
 }
 
+// add new fruit list itme.
+const fruitLists = document.querySelector('.fruits');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const newFruit = event.target.querySelector('#fruit-to-add').value;
-  const newFruitDescription = event.target.querySelector('.fruit-describe').value;
-  
-  const ul = document.querySelector('.fruits');
-  
   const li = document.createElement('li');
   li.classList.add('fruit');
   
-  const div = document.createElement('div');
-  div.classList.add('fruit-details');
+  const nameFruit = document.getElementById('fruit-to-add').value;
+  const descFruit = document.getElementById('description').value;
   
-  const paraFruitName = document.createElement('p');
-  paraFruitName.textContent = newFruit;
-  div.appendChild(paraFruitName);
+  const namePara = document.createElement('p');
+  namePara.textContent = nameFruit;
+  li.appendChild(namePara);
   
-  const paraFruitDescription = document.createElement('p');
-  paraFruitDescription.textContent = newFruitDescription;
-  paraFruitDescription.style.fontStyle = 'italic';
+  const descPara = document.createElement('p');
+  descPara.textContent = descFruit;
+  // Show the fruit description in italics on the next line
+  descPara.style.fontStyle = 'italic';
   
-  div.appendChild(paraFruitDescription);
-  li.appendChild(div);
-  addBtns(li);
-  ul.appendChild(li);
-
-  // clean inputs
+  li.appendChild(descPara);
+  addBtnToLi(li);
+  fruitLists.appendChild(li);
+  
   form.querySelector('#fruit-to-add').value = '';
-  form.querySelector('.fruit-describe').value = '';
+  form.querySelector('#description').value = '';
   form.querySelector('#fruit-to-add').focus();
 });
 
+// remove fruit list item.
+fruitLists.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-btn')) {
+    const removeFruit = event.target.parentElement;
+    fruitLists.removeChild(removeFruit);
+  }
+});
 
-
-filter.addEventListener('keyup', (e) => {
-  const text = e.target.value.toLowerCase();
+// Create a filter that shows only those fruits whose either name or description or both matches the entered text
+const filter = document.getElementById('filter');
+filter.addEventListener('keyup', (event) => {
+  const text = event.target.value.toLowerCase();
   const fruits = document.querySelectorAll('.fruit');
-
-  for(let i = 0; i < fruits.length; i++) {
-    const fruitName = fruits[i].firstChild.textContent.toLocaleLowerCase();
-    if(fruitName.indexOf(text) === -1) {
-      fruits[i].style.display = 'none';
+  for (const fruit of fruits) {
+    const name = fruit.firstChild.textContent.toLowerCase();
+    
+    if (name.indexOf(text) !== -1) {
+      fruit.style.display = 'flex';
     } else {
-      fruits[i].style.display = 'flex';
+      fruit.style.display = 'none';
     }
   }
 });
