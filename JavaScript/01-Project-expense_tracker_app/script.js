b8e9c4c32a1747fa30ace8d192e4b756aa0cd287
ghost <keshav.bgir.980@gmail.com>
2025-08-28 14:11:43 +0530

    01-javaScript project html code update add new footer tag and fix some issues [advance javaScript]

--- JavaScript/01-Project-expense_tracker_app/script.js
+++ JavaScript/01-Project-expense_tracker_app/script.js
@@ -3,7 +3,7 @@ import { Expense, getFromLocalStorage, setToLocalStorage } from "./utils.js";
 const key = 'expenses';
 const expenses = getFromLocalStorage(key);
 
-window.addEventListener('DOMContentLoaded', initial)
+window.addEventListener('DOMContentLoaded', initial);
 const form = document.querySelector('form');
 
 function initial(){
@@ -59,14 +59,15 @@ function addActionButtons(tr,  expense_obj){
   const td = document.createElement('td');
 
   const editBtn = document.createElement('button');
-  editBtn.classList.add('btn btn-warning btn-sm');
+  // editBtn.classList.add('btn btn-warning btn-sm');
+  editBtn.className = 'btn btn-primary btn-sm';
   editBtn.dataset.editId = expense_obj.id;
   editBtn.textContent = 'Edit';
   editBtn.addEventListener('click', () => updateExpense(expense_obj));
   
   const deleteBtn = document.createElement('button');
   deleteBtn.dataset.deleteId = expense_obj.id;
-  deleteBtn.classList.add('btn btn-danger btn-sm');
+  deleteBtn.className = 'btn btn-danger btn-sm';
   deleteBtn.textContent = 'Delete';
   deleteBtn.addEventListener('click', () => deleteExpense(expense_obj.id, tr));
 
